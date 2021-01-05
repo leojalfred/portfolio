857ae77772e033dcf5c6b8eba1c416a3704ab7a2
Leo J Alfred <leojalfred@gmail.com>
2021-01-05 14:35:42 -0600

    Finishes initial navbar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,47 @@
+import profile from './images/profile.jpg'
 import './App.scss'
 
 export default function App() {
-  return <div className="App"></div>
+  const handleClick = () => window.scrollTo({ top: 0, behavior: 'smooth' })
+
+  return (
+    <div className="app">
+      <nav className="navbar">
+        <div className="navbar__links">
+          <div className="navbar__link-item">Bio</div>
+          <div className="navbar__link-item">Skills</div>
+          <div className="navbar__link-item">Portfolio</div>
+          <div className="navbar__link-item">Experience</div>
+          <div className="navbar__link-item">Contact</div>
+        </div>
+        <img
+          className="navbar__home"
+          src={profile}
+          alt="Home"
+          onClick={handleClick}
+        />
+        <div className="navbar__socials">
+          <a
+            className="navbar__social-item"
+            href="https://www.linkedin.com/in/leojalfred/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <i class="fi fi-linkedin"></i>
+          </a>
+          <a
+            className="navbar__social-item"
+            href="https://github.com/leojalfred"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <i class="fi fi-github"></i>
+          </a>
+        </div>
+      </nav>
+      <div className="main">
+        <h1>big ol cock</h1>
+      </div>
+    </div>
+  )
 }
