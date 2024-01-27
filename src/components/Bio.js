f49b99524295edc75a3b8cbdf2d85dab16012b50
Leo Alfred <leojalfred@gmail.com>
2024-01-26 23:32:11 -0600

    Update Bio

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -1,4 +1,3 @@
-import resume from '../documents/resume.pdf'
 import profile from '../images/profile.jpg'
 import scrollTo from '../utils/scrollTo'
 import './Bio.scss'
@@ -13,36 +12,35 @@ export default function Bio({ bio, contact }) {
       <div className="bio__text">
         <h2 className="bio__heading">About me</h2>
         <p>
-          Whether you need someone to develop entire applications solo or
-          integrate seamlessly into a team, I’m your guy. I have eight years of
-          serious full stack development experience under my belt and a
-          significant professional background in many other areas. On a weekly
-          basis I can switch between full stack web development, mobile app
-          development, web design, UI/UX design, SEO, project and personnel
-          management, website management and maintenance, quality assurance,
-          information technology support, and general consulting.
+          Navigating the web should be seamless, not aggravating. As a dedicated
+          full stack developer with a decade of experience, I specialize in
+          creating beautiful products with intuitive interactions.
         </p>
         <p>
-          I'm passionate about applying technology and design to solve
-          real-world problems and love to spend my time learning new skills and
-          developing websites and mobile applications. I'm always looking to
-          tackle new and intriguing technical challenges, so if you think I
-          could be of help, don’t hesitate to{' '}
+          From freelancing and web development competitions in my early years to
+          my current role as a Senior Software Engineer on Kipsu’s frontend
+          team, I've consistently crafted optimized, responsive, satisfying
+          applications. I’ve worked independently, as a contractor, and spent
+          time at agencies doing everything from mobile application development
+          to design to data architecture, but I’ve found it most gratifying to
+          work in-house at SaaS companies as a full stack developer. I love to
+          solve complex problems from ideation through execution, with a focus
+          on delivering exceptional user experiences.
+        </p>
+        <p>
+          At Kipsu, I led my team to automate a core use case with AI, improving
+          end user productivity by 20%. I work efficiently, developing Kipsu’s
+          React Native SDK in half the estimated time and creating a demo for
+          one of Br8kthru’s clients in just 5% of the quoted time.
+        </p>
+        <p>
+          I'm always looking to tackle new and intriguing technical challenges.
+          If you think I could be of help, please{' '}
           <button className="bio__contact" onClick={scrollTo(contact)}>
             reach out
-          </button>
-          .
+          </button>{' '}
+          for a deeper conversation.
         </p>
-        <div className="bio__resume-wrapper">
-          <a
-            className="bio__resume"
-            href={resume}
-            target="_blank"
-            rel="noreferrer"
-          >
-            See résumé
-          </a>
-        </div>
       </div>
     </div>
   )
